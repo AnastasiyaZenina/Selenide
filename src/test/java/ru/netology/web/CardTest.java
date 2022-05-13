20ddb3542de77a8ce81853739f1ad18f8a268159
Anastasiya Zenina <stasya.soloveva.2014@mail.ru>
2022-05-13 10:18:08 +0300

    change test

--- src/test/java/ru/netology/web/CardTest.java
+++ src/test/java/ru/netology/web/CardTest.java
@@ -1,24 +1,50 @@
 package ru.netology.web;
 
-import com.codeborne.selenide.Condition;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
+
 public class CardTest {
 
+    public static String meetingDate() {
+        LocalDate date = LocalDate.now();
+        return LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
     @Test
     void successfulCardOrder() {
-        open("http://localhost:9999/");
         $("[placeholder='Город']").setValue("Чита");
-        $("[placeholder='Дата встречи']").doubleClick().append("20.05.2022");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.CONTROL + "a");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.DELETE);
+        $("[placeholder='Дата встречи']").setValue(meetingDate());
         $(By.name("name")).setValue("Татьяна Соловьева");
+        $(By.name("phone")).setValue("+79003003496");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(withText("Встреча успешно забронирована на")).should(visible, Duration.ofSeconds(15));
+    }
+    @Test
+    void successfulCardOrder2() {
+        $("[placeholder='Город']").setValue("Чита");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.CONTROL + "a");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.DELETE);
+        $("[placeholder='Дата встречи']").setValue(meetingDate());
+        $(By.name("name")).setValue("Татьяна Иванова-Петрова");
         $(By.name("phone")).setValue("+79003003496");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
